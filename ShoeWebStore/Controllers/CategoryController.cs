69b7c97cb0f172bb0bacc6b27061b59b7930ca0c
karlafehir <fehirkarla@gmail.com>
2024-03-27 13:09:46 +0100

    Create new category layout, tempData notification

--- ShoeWebStore/Controllers/CategoryController.cs
+++ ShoeWebStore/Controllers/CategoryController.cs
@@ -12,11 +12,34 @@ public class CategoryController : Controller
     {
         _unitOfWork = unitOfWork;
     }
-    
+
     public IActionResult Index()
     {
         List<Category> categoryList = _unitOfWork.Category.GetAll().ToList();
         return View(categoryList);
     }
 
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        if (category.Name.Length < 4)
+        {
+            ModelState.AddModelError("key", "Name must be longer than 4 characters.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Category.Add(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category created successfully!";
+            return RedirectToAction("Index", "Category");
+        }
+
+        return View();
+    }
 }
