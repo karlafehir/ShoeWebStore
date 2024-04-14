bb7667cac07509441d05e3e2cc45574637ee1d13
karlafehir <fehirkarla@gmail.com>
2024-04-14 18:21:56 +0200

    Added product details page, show products on home page

--- ShoeWebStore/Controllers/HomeController.cs
+++ ShoeWebStore/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using ShoeWebStore.DataAccess.Repository.IRepository;
 using ShoeWebStore.Models;
+using ShoeWebStore.Models.Models;
 
 namespace ShoeWebStore.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+        return View(productList);
     }
 
     public IActionResult Privacy()
@@ -27,5 +32,11 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Details(int productId)
+    {
+        Product product = _unitOfWork.Product.Get(p => p.Id == productId, includeProperties: "Category");
+        return View(product);
     }
 }
